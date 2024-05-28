c6325469abfc8b663ffe93ceedaf4ccde2b5afed
J-Pantaroto <jhonatanpantarto@gmail.com>
2024-05-28 01:23:53 -0300

    jesus

--- scriptHP.js
+++ scriptHP.js
@@ -1,20 +1,70 @@
-document.getElementById('show-login').addEventListener("click",function(){
-    document.querySelector(".janela-login").classList.add("active");
-});
-document.querySelector(".hiden-login").addEventListener("click",function(){
-    document.querySelector(".janela-login").classList.remove("active");
+const showSignup = document.getElementById('show-signup');
+const janelaSignup = document.querySelector(".janela-signup");
+const btnHidden = document.querySelector(".hiden-signup");
+const nomeInput = document.getElementById('nome');
+const loginInput = document.querySelector('.form-group input[name="login"]');
+const fade = document.querySelector("#fade");
+const showLogin = document.getElementsByClassName('show-login');
+const janelaLogin = document.querySelector(".janela-login");
+const btnHidden2 = document.querySelector(".hiden-login");
+
+
+
+showLogin.addEventListener('click', function() {
+    janelaLogin.classList.add('active');
+    fade.classList.add('active');
+    document.body.classList.add('blur');
+    loginInput.focus();
 });
 
+fade.addEventListener('click', function() {
+    janelaLogin.classList.remove('active');
+    fade.classList.remove('active');
+    document.body.classList.remove('blur');
+});
+
+btnHidden2.addEventListener('click', function() {
+    janelaLogin.classList.remove('active');
+    fade.classList.remove('active');
+    document.body.classList.remove('blur');
+});
+
+
+
+showSignup.addEventListener('click', function() {
+    janelaSignup.classList.add('active');
+    fade.classList.add('active');
+    document.body.classList.add('blur');
+    nomeInput.focus();
+});
+
+fade.addEventListener('click', function() {
+    janelaSignup.classList.remove('active');
+    fade.classList.remove('active');
+    document.body.classList.remove('blur');
+});
+
+btnHidden.addEventListener('click', function() {
+    janelaSignup.classList.remove('active');
+    fade.classList.remove('active');
+    document.body.classList.remove('blur');
+});
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    handleSubmit(event);
+});
 
 function handleSubmit(event) {
     event.preventDefault();
     var nome = document.getElementById('nome').value;
+    var login = document.getElementById('login').value;
     var cpf = document.getElementById('cpf').value;
     var email = document.getElementById('email').value;
     var senha = document.getElementById('password').value;
     var senha2 = document.getElementById('confirm-password').value;
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //regex
-    if (!nome || !cpf || !email || !senha || !senha2) {
+    if (!nome || !cpf || !login || !email || !senha || !senha2) {
         Swal.fire({
             title: "Erro!",
             text: "Todos os campos são obrigatórios.",
@@ -43,8 +93,14 @@ function handleSubmit(event) {
         title: "Bom trabalho!",
         text: "Cadastro realizado com sucesso!",
         icon: "success"
+    }).then(() => {
+        janela.classList.remove('active');
+        fade.classList.remove('active');
+        document.body.classList.remove('blur');
+
     });
 }
+
 function formatarCPF(cpf) {
     cpf = cpf.replace(/\D/g, '');
     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
@@ -53,15 +109,10 @@ function formatarCPF(cpf) {
     return cpf;
 }
 
-
 function atualizarCPF(event) {
     var input = event.target;
     var cpf = input.value;
     input.value = formatarCPF(cpf);
 }
 
-
 document.getElementById('cpf').addEventListener('input', atualizarCPF);
-document.getElementById('submit').addEventListener("click", handleSubmit);
-
-
