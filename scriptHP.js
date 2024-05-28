afe6141736c6ccbbd1b62a930bcf728e63920d27
J-Pantaroto <jhonatanpantaroto@gmail.com>
2024-05-28 17:30:27 -0400

    versao final aleluia

--- scriptHP.js
+++ scriptHP.js
@@ -2,19 +2,45 @@ const showSignup = document.getElementById('show-signup');
 const janelaSignup = document.querySelector(".janela-signup");
 const btnHidden = document.querySelector(".hiden-signup");
 const nomeInput = document.getElementById('nome');
-const loginInput = document.querySelector('.form-group input[name="login"]');
 const fade = document.querySelector("#fade");
-const showLogin = document.getElementsByClassName('show-login');
+const showLogin = document.getElementById('show-login');
 const janelaLogin = document.querySelector(".janela-login");
 const btnHidden2 = document.querySelector(".hiden-login");
+const mostrarSenhaCadastro = document.getElementById('show-password');
+const mostrarSenhaLogin = document.getElementById('show-passwordLogin');
+const usuarios = [];
+
+class Usuario {
+    constructor(nome, login, cpf, email, senha, sexo) {
+        this.nome = nome;
+        this.login = login;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.sexo = sexo;
+    }
+    //funcao para LOGIN
+    static validarLogin(login, senha, usuariosRegistrados) {
+        const usuario = usuariosRegistrados.find(user => user.login === login && user.senha === senha);
+        return usuario ? true : false;
+    }
+    
+}
+//funcao para CADASTRO
+function registrarUsuario(nome, login, cpf, email, senha, sexo) {
+    const novoUsuario = new Usuario(nome, login, cpf, email, senha, sexo);
+    usuarios.push(novoUsuario);
+    console.log(usuarios);
+}
 
 
-
-showLogin.addEventListener('click', function() {
+function abrirLogin(){
     janelaLogin.classList.add('active');
     fade.classList.add('active');
     document.body.classList.add('blur');
-    loginInput.focus();
+}
+showLogin.addEventListener('click', function() {
+    abrirLogin();
 });
 
 fade.addEventListener('click', function() {
@@ -28,8 +54,6 @@ btnHidden2.addEventListener('click', function() {
     fade.classList.remove('active');
     document.body.classList.remove('blur');
 });
-
-
 
 showSignup.addEventListener('click', function() {
     janelaSignup.classList.add('active');
@@ -53,6 +77,10 @@ btnHidden.addEventListener('click', function() {
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     handleSubmit(event);
+});
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    handleLogin(event);
 });
 
 function handleSubmit(event) {
@@ -90,16 +118,57 @@ function handleSubmit(event) {
         return;
     }
     Swal.fire({
-        title: "Bom trabalho!",
+        title: "Sucesso!",
         text: "Cadastro realizado com sucesso!",
         icon: "success"
     }).then(() => {
-        janela.classList.remove('active');
+        janelaSignup.classList.remove('active');
         fade.classList.remove('active');
         document.body.classList.remove('blur');
-
+        const nome = document.getElementById('nome').value;
+        const login = document.getElementById('login').value;
+        const cpf = document.getElementById('cpf').value;
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('password').value;
+        const sexo = document.querySelector('input[name="sexo"]:checked').value;
+        registrarUsuario(nome, login, cpf, email, senha, sexo);
+        abrirLogin();
     });
 }
+
+function handleLogin(event) {
+    event.preventDefault();
+    const login = document.getElementById('login-entrar').value;
+    const senha = document.getElementById('senha-entrar').value;
+
+    if (Usuario.validarLogin(login, senha, usuarios)) {
+        Swal.fire({
+            title: "Sucesso!",
+            text: "Login realizado com sucesso!",
+            icon: "success"
+        }).then(() => {
+            janelaLogin.classList.remove('active');
+            fade.classList.remove('active');
+            document.body.classList.remove('blur');
+            window.location.href = 'shop.html';
+        });;
+
+    } else {
+        Swal.fire({
+            title: "Erro!",
+            text: "Login ou senha incorretos.",
+            icon: "error"
+        });
+    }
+}
+
+
+
+
+
+
+
+
 
 function formatarCPF(cpf) {
     cpf = cpf.replace(/\D/g, '');
@@ -115,4 +184,38 @@ function atualizarCPF(event) {
     input.value = formatarCPF(cpf);
 }
 
+
+
+
+
+
+
+
+
+
+
+function mostrarSenha() {
+    var senha = document.getElementById('password');
+    var senha2 = document.getElementById('confirm-password');
+    var senhaLogin = document.getElementById('senha-entrar');
+    mostrarSenhaCadastro.addEventListener('change', function() {
+        if (mostrarSenhaCadastro.checked) {
+            senha.type = 'text';
+            senha2.type = 'text';
+        } else {
+            senha.type = 'password';
+            senha2.type = 'password';
+        }
+    });
+    mostrarSenhaLogin.addEventListener('change', function() {
+        if (mostrarSenhaLogin.checked) {
+            senhaLogin.type = 'text';
+        } else {
+            senhaLogin.type = 'password';
+        }
+    });
+}
 document.getElementById('cpf').addEventListener('input', atualizarCPF);
+document.addEventListener('DOMContentLoaded', function() {
+mostrarSenha();
+});
